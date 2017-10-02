b6dc767b4bdddc25224194c6ab1b69078b7ed0cd
Mi&Jack <51164500190@stu.ecnu.edu.cn>
2017-10-02 21:44:34 +0800

    creat project

---
+++ src/main/java/com/mijack/SpringBootBlogSystem/SpringBootBlogSystemApplication.java
@@ -0,0 +1,12 @@
+package com.mijack.SpringBootBlogSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogSystemApplication.class, args);
+	}
+}
